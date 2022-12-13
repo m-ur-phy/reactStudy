5b7b82f2483d9393835e36e2ae9ea26f30fc4ac2
m-ur-phy <ooiillppaaiinntt@gmail.com>
2022-12-13 12:03:42 +0900

    class 방식 react

--- academy/project/my-app/src/App.js
+++ academy/project/my-app/src/App.js
@@ -10,26 +10,62 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-				
-      subject:{title:'WEB' , sub:'world wide web!'},
-      contents:[
-        {id:1, title:'HTML', desc:'설명글1'},
-        {id:2, title:'CSS', desc:'설명글2'},
-        {id:3, title:'JavaScript', desc:'설명글3'},
-        {id:4, title:'React', desc:'설명글4'}
-      ]
-  }
+        mode:'read',
+        selected_content_id:1,
+        welcome:{title:'welcome' , desc:'hello React!'},
+
+        subject:{title:'WEB' , sub:'world wide web!'},
+        contents:[
+          {id:1, title:'HTML', desc:'설명글1'},
+          {id:2, title:'CSS', desc:'설명글2'},
+          {id:3, title:'JavaScript', desc:'설명글3'},
+          {id:4, title:'Vue', desc:'설명글4'}
+        ]
+    }
   }
 
   render(){
+    var _title, _desc=null;
+    if(this.state.mode === 'welcome'){
+       _title = this.state.welcome.title;
+       _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+       var i=0;
+       while(i<this.state.contents.length){
+           var data = this.state.contents[i];
+           if(data.id === this.state.selected_content_id){
+             _title = data.title;
+             _desc = data.desc;
+             break;
+           }
+         i+=1;
+       }
+    } 
     return (
       <div className="App">
             <Subject 
               title={this.state.subject.title} 
-              sub={this.state.subject.sub}>
+              sub={this.state.subject.sub}
+             
+              onChangePage={function(){
+                this.setState({
+                  mode:'welcome'
+                });
+              }.bind(this)}
+            >
             </Subject>
-            <Gnb data={this.state.contents}></Gnb>
-            <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+
+            <Gnb 
+              onChangePage={function(id){
+                this.setState({
+                  mode:'read',
+                  selected_content_id:Number(id)  //id값이 문자 "1" 라서 숫자로 변환
+                });
+              }.bind(this)}
+            data={this.state.contents}>
+            </Gnb>
+
+            <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- academy/project/my-app/src/components/Gnb.js
+++ academy/project/my-app/src/components/Gnb.js
@@ -3,16 +3,33 @@ import './css/Gnb.css'
 
 class Gnb extends Component {
     render(){
-        var aaa = [<p>단락요소1입니다.</p>, <p>단락요소2입니다.</p>, <p>단락요소3입니다.</p>, <p>단락요소4입니다.</p>];
-        var bbb = <p>단락요소긔</p>;
 
         var lists = [];  
         var data = this.props.data;
         var i=0;
         
         while(i<data.length){
-            lists.push(<li key={data[i].id}><a href={data[i].id+".html"} title={data[i].desc}>{data[i].title}</a></li>);
-            i+=1;
+          lists.push(
+              <li key={data[i].id}>
+                  <a href={data[i].id+".html"} title={data[i].desc} 
+                    data-id={data[i].id} 
+                    onClick={function(e){
+                      e.preventDefault();
+                      this.props.onChangePage(e.target.dataset.id);
+                  }.bind(this)}
+                  >
+  
+                  {/* <a href={data[i].id+".html"} title={data[i].desc} 
+                    onClick={function(id, e){
+                      e.preventDefault();
+                      this.props.onChangePage(id);
+                  }.bind(this, data[i].id)}
+                  > */}
+  
+                      {data[i].title}
+                  </a>
+              </li>);
+          i+=1;
         }
 
      // for(i=0; i<data.length; i++){
@@ -22,11 +39,8 @@ class Gnb extends Component {
       return (
         <nav>
           <ul>
-              {lists}
+          {lists}
           </ul>
-          <div>{aaa[0]}</div>
-          <div>{aaa[1]}</div>
-          <div>{bbb}</div>
         </nav>
       );
     }
