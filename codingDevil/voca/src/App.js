75cd63cfc14eb706c2696030e77ebffe08db841a
m-ur-phy <ooiillppaaiinntt@gmail.com>
2022-12-15 16:03:21 +0900

    state, props

--- codingDevil/voca/src/App.js
+++ codingDevil/voca/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Hello from './component/Hello'
-import Welcome from './component/Welcome';
-import styles from './App.module.css';
 
 function App() {
 
   return (
     <div className="App">
-      <Hello />
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
   );
 }
--- codingDevil/voca/src/component/Hello.js
+++ codingDevil/voca/src/component/Hello.js
@@ -1,13 +1,16 @@
-export default function Hello() {
-    // function showText(e) {
-    //     console.log(e.target.value)
-    // }
+import { useState } from "react"; 
+import UserName from "./UserName";
+
+export default function Hello({age}) { // 전달해준 값이 들어온다
+    const [name, setName] = useState('Mike'); 
+    const msg = age > 19 ? '성인입니다' : '미성년자 입니다';
     return (
         <>
-        <h1>Hello</h1>
-        <input type="text" onChange={ e => {
-            console.log(e.target.value);
-        }}/>
+            <h1 id="name">{name}{age} : {msg}</h1>
+            <UserName name={name}></UserName>
+            <button onClick={() => {
+                setName(name === "Murphy" ? "Harry" : "Murphy");
+            }}>Change</button>
         </>
     )
 }
